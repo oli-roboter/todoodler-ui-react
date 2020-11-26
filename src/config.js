0b8d98fa6f99aeaaef328b3e76a885cbe1f2ac4d
Oliver Wagner <oliverthomas.wagner@gmail.com>
2020-11-26 12:16:21 +0000

    added systemvars flag to dotenv webpack

--- src/config.js
+++ src/config.js
@@ -5,6 +5,9 @@ dotenv.config();
 const AUTH = process.env.AUTH_BAKEND;
 const TODO = process.env.TODOODLER_BACKEND;
 
+console.log("AUTH:", AUTH);
+console.log("TODO:", TODO);
+
 export {
   AUTH,
   TODO,
--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new Dotenv(),
+    new Dotenv({ systemvars: true }),
   ],
 };
